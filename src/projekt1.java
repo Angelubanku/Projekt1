6d9a8041ea73be9a872cd9c71daf8d625478ba59
Angelubanjku <Angeluba73@gmail.com>
2022-10-24 15:10:45 +0200

    first commit

---
+++ src/projekt1.java
@@ -0,0 +1,15 @@
+
+public class projekt1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("xxxxx");
+		System.out.println("x    x");
+		System.out.println("x    x");
+		System.out.println("xxxxx");
+		
+
+	}
+
+}
